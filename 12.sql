7610f9c5cd7cefcffa1bc1ee441368c634154846
Ali MAKRI <github@makri.fr>
2023-03-28 16:30:44 +0200

    16:30

--- 12.sql
+++ 12.sql
@@ -1,6 +1,12 @@
--- 
-
+-- Proc stoc Filtrer par ville, par initial de nom
+CREATE PROC ListePersonne(@ville nvarchar(50), @initiale char)
+AS
 SELECT Person.Person.BusinessEntityID, Person.Person.FirstName, Person.Person.LastName, Person.Address.City
 FROM     Person.Address INNER JOIN
                   Person.BusinessEntityAddress ON Person.Address.AddressID = Person.BusinessEntityAddress.AddressID INNER JOIN
                   Person.Person ON Person.BusinessEntityAddress.BusinessEntityID = Person.Person.BusinessEntityID
+GO
+EXEC ListePersonne
+Exec ListePersonne 'Seattle'
+Exec ListePersonne 'Seattle', 'A'
+Exec ListePersonne @initiale = 'B'
