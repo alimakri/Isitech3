3a0a8d03ff0d42b4a37ef59861e71fe4f6581129
Ali MAKRI <github@makri.fr>
2023-03-28 17:15:59 +0200

    17:15

--- 13.sql
+++ 13.sql
@@ -1,15 +1,5 @@
-select * from bd1.dbo.Personne 
-DECLARE @xml xml = '<personnes>
-  <personne id="1">
-      <nom>Dupont</nom>
-      <prenom>Pierre</prenom>
-  </personne>
-  <personne id="2">
-      <nom>Zola</nom>
-      <prenom>Emile</prenom>
-  </personne>
-</personnes>'
-
+CREATE PROC AjoutPersonneXML(@xml xml)
+AS
 SELECT  
 	T.N.value('@id[1]', 'int') id,  
 	T.N.value('nom[1]', 'nvarchar(MAX)') nom,  
@@ -19,5 +9,16 @@ FROM
 	@xml.nodes('/personnes/personne') T(N)  
 
 insert BD1.dbo.Personne (nom, prenom) select  Nom, Prenom from #t
+GO
+Exec AjoutPersonneXML '<personnes>
+  <personne id="1">
+      <nom>Dupont</nom>
+      <prenom>Pierre</prenom>
+  </personne>
+  <personne id="2">
+      <nom>Zola</nom>
+      <prenom>Emile</prenom>
+  </personne>
+</personnes>'
 
 select * from bd1.dbo.Personne 
---
+++ 14.sql
@@ -0,0 +1,5 @@
+Exec OperationXML '
+	<operations>
+		<doublon op="delete" nom="Dupont" prenom="Pierre" />
+		<doublon op="delete" nom="Zola" prenom="Emile" />
+	</operations>'
