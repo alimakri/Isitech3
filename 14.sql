80742d522f916e576d9c45df8415fa5fde2a9764
Ali MAKRI <github@makri.fr>
2023-03-29 09:25:18 +0200

    09:25

--- 14.sql
+++ 14.sql
@@ -1,3 +1,15 @@
+CREATE PROC OperationXML(@xml xml)
+AS
+SELECT  
+	T.N.value('@op[1]', 'nvarchar(MAX)') id,  
+	T.N.value('@nom[1]', 'nvarchar(MAX)') nom,  
+	T.N.value('@prenom[1]', 'nvarchar(MAX)') prenom
+INTO #t
+FROM   
+	@xml.nodes('/operations/doublon') T(N)  
+
+--insert BD1.dbo.Personne (nom, prenom) select  Nom, Prenom from #t
+GO
 Exec OperationXML '
 	<operations>
 		<doublon op="delete" nom="Dupont" prenom="Pierre" />
